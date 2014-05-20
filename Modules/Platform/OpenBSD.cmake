0884710716801c3aba2748aada5873975a1b6f11
Rolf Eike Beer <eike@sf-mail.de>
2014-05-20 17:33:20 +0200

    Revert topic OpenBSD-platform
    This causes some test failures where the proper way of addressing them is
    currently unknown.

--- Modules/Platform/OpenBSD.cmake
+++ Modules/Platform/OpenBSD.cmake
@@ -1,7 +1,5 @@
 include(Platform/NetBSD)
 
-unset(CMAKE_SHARED_LIBRARY_SONAME_C_FLAG)
-
 # On OpenBSD, the compile time linker does not share it's configuration with
 # the runtime linker.  This will extract the library search paths from the
 # system's ld.so.hints file which will allow CMake to set the appropriate
@@ -22,7 +20,3 @@ set_property(GLOBAL PROPERTY FIND_LIBRARY_USE_OPENBSD_VERSIONING 1)
 # OpenBSD policy requires that shared libraries be installed without
 # executable permission.
 set(CMAKE_INSTALL_SO_NO_EXE 1)
-
-# Shared libraries with no builtin soname may not be linked safely by
-# specifying the file path.
-set(CMAKE_PLATFORM_USES_PATH_WHEN_NO_SONAME 1)
